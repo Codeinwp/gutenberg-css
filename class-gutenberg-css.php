3348b0bb98ab468837986d85c50f2b6594a7162c
Hardeep Asrani <hardeepasrani@gmail.com>
2019-12-03 14:15:26 +0530

    Fix issue with ServerSideRenderer blocks
    This makes sure the blocks which use ServerSideRenderer work just fine.

--- class-gutenberg-css.php
+++ class-gutenberg-css.php
@@ -30,7 +30,7 @@ if ( ! class_exists( '\ThemeIsle\GutenbergCSS' ) ) {
 		public function init() {
 			add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_editor_assets' ) );
 			add_action( 'wp_head', array( $this, 'render_server_side_css' ) );
-			add_action( 'wp_loaded', array( $this, 'add_attributes_to_registered_blocks' ), 100 );
+			add_action( 'init', array( $this, 'add_attributes_to_blocks' ), 11 );
 		}
 
 		/**
@@ -159,20 +159,22 @@ if ( ! class_exists( '\ThemeIsle\GutenbergCSS' ) ) {
 		 * Adds the `hasCustomCSS` and `customCSS` attributes to all blocks, to avoid `Invalid parameter(s): attributes`
 		 * error in Gutenberg.
 		 *
-		 * @hooked wp_loaded, 100
+		 * @since   1.0.3
+		 * @access  public
 		 */
-		public function add_attributes_to_registered_blocks() {
+		public function add_attributes_to_blocks() {
 
-			$registered_blocks = WP_Block_Type_Registry::get_instance()->get_all_registered();
+			$registered_blocks = \WP_Block_Type_Registry::get_instance()->get_all_registered();
 
 			foreach( $registered_blocks as $name => $block ) {
 				$block->attributes['hasCustomCSS'] = array(
 					'type'    => 'boolean',
-					'default' => false,
+					'default' => false
 				);
-				$block->attributes['customCSS']    = array(
+
+				$block->attributes['customCSS'] = array(
 					'type'    => 'string',
-					'default' => '',
+					'default' => ''
 				);
 			}
 		}
