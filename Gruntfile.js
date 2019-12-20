a5977f97679dc0a00f982d41dead57168827fba8
selul <marius.cristea@vertistudio.com>
2019-12-20 17:13:04 +0200

    release: fix version replacement routine

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,11 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-version');
 	grunt.initConfig({
 		version: {
+			options: {
+				flags: ''
+			},
 			project: {
-				src: ['package.json', 'package-lock.json', 'composer.json', 'composer.lock']
+				src: ['package.json', 'composer.json', 'package-lock.json']
 			}
 		}
 	});
