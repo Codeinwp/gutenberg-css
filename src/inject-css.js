90bd5339f08d1cb758ecb6fd222426f55b3d59ca
Robert <soarerobert17@gmail.com>
2021-02-03 13:14:10 +0000

    Refactor Inject CSS

--- src/inject-css.js
+++ src/inject-css.js
@@ -1,3 +1,4 @@
+
 /**
  * WordPress dependencies.
  */
@@ -9,6 +10,10 @@ const {
 	select,
 	subscribe
 } = wp.data;
+
+const {
+	flattenDeep
+} = lodash;
 
 const addStyle = style => {
 	let element = document.getElementById( 'themeisle-css-editor-styles' );
@@ -27,35 +32,62 @@ const addStyle = style => {
 	return element.textContent = style;
 };
 
-let style = '';
+/*
+	This function will get the `customCss` value from all the blocks and its children
+*/
+const getCustomCssFromBlocks = ( blocks, reusableBlocks ) => {
+	if ( ! blocks ) {
+		return '';
+	}
 
-const cycleBlocks = ( blocks, reusableBlocks ) => {
-	blocks.forEach( block => {
-		if ( block.attributes.hasCustomCSS ) {
-			if ( block.attributes.customCSS && ( null !== block.attributes.customCSS ) ) {
-				style += block.attributes.customCSS + '\n';
-			}
-		}
-
+	// Return the block and its children. The result is an array deeply nested that match the structure of the block in the editor.
+	const getChildrensFromBlock = ( block ) => {
+		const childrends = [];
 		if ( 'core/block' === block.name && null !== reusableBlocks ) {
-			let reBlocks = reusableBlocks.find( i => block.attributes.ref === i.id );
-			if ( reBlocks ) {
-				reBlocks = parse( reBlocks.content.raw );
-				cycleBlocks( reBlocks, reusableBlocks );
+			const reBlocks = reusableBlocks.find( i => block.attributes.ref === i.id );
+			if ( reBlocks && reBlocks.content ) {
+				childrends.push(  parse(  reBlocks.content.raw || reBlocks.content ).map( ( children ) => [ children, getChildrensFromBlock( children ) ])  );
 			};
 		}
 
 		if ( undefined !== block.innerBlocks && 0 < ( block.innerBlocks ).length ) {
-			cycleBlocks( block.innerBlocks, reusableBlocks );
+			childrends.push( block.innerBlocks.map( ( children ) => [ children, getChildrensFromBlock( children ) ]) );
 		}
+
+		return childrends;
+	};
+
+	// Get all the blocks and their children
+	const allBlocks = blocks.map( ( block ) => {
+		return [ block, getChildrensFromBlock( block ) ];
 	});
+
+	// Transform the deply nested array in a simple one and then get the `customCss` value where is the case
+	const extractCustomCss = flattenDeep( allBlocks ).map( ( block ) => {
+		if ( block.attributes && block.attributes.hasCustomCSS ) {
+			if ( block.attributes.customCSS && ( null !== block.attributes.customCSS ) ) {
+				return block.attributes.customCSS + '\n';
+			}
+		}
+		return '';
+	});
+
+	// Build the global style
+	const style = extractCustomCss.reduce( ( acc, localStyle ) => acc + localStyle, '' );
+
+	// For debugging
+	// console.log( 'Get all the block', allBlocks );
+	// console.log( 'Extract customCss', extractCustomCss );
+	// console.log( 'Final Result\n', style );
+
+
+	return style;
 };
 
 const subscribed = subscribe( () => {
-	style = '';
 	const { getBlocks } = select( 'core/block-editor' ) || select( 'core/editor' );
 	const blocks = getBlocks();
 	const reusableBlocks = select( 'core' ).getEntityRecords( 'postType', 'wp_block' );
-	cycleBlocks( blocks, reusableBlocks );
-	addStyle( style );
+	const blocksStyle = getCustomCssFromBlocks( blocks, reusableBlocks );
+	addStyle( blocksStyle );
 });
